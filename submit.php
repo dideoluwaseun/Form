5e181475e55aa767d5172ecd52d264c71f5764e5
Oyinloye Oluwaseun <88078469+dideoluwaseun@users.noreply.github.com>
2021-10-02 12:25:31 +0100

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+   <h4>Successfully Submitted Form Data</h4>
+   <p>Your input was received as:</p>
+
+   First Name: <?php echo $_POST["fName"]; ?><br>
+   Last Name: <?php echo $_POST["lName"]; ?><br>
+   Gender: <?php echo $_POST["Gender"]; ?><br>
+   Email address: <?php echo $_POST["email"]; ?><br>
+   Phone Number: <?php echo $_POST["phone"]; ?>
+
+</body>
+</html>
